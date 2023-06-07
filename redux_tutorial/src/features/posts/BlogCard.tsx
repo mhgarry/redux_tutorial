b9008440a4cf2b384893f3cb9d3bc87701f27289
Matt <mhgarry92@gmail.com>
2023-06-07 10:14:11 -0400

    render blogcard on homepage

---
+++ redux_tutorial/src/features/posts/BlogCard.tsx
@@ -0,0 +1,43 @@
+const BlogCard = (props: { posts:any }) => {
+  return(
+    <>
+      {props.posts.map((post: any) => {
+        return (
+          <section className="card" key={post.id}>
+            <div className="card-header">
+              <span className="edit-icon">✎</span>
+              <h1 className="delete-icon">x</h1>
+            </div>
+            <section className="card-body">
+              <h1 className="card-title">
+                {post.title.length > 150
+                  ? post.title.slice(0, 150) + "..."
+                  : post.title}
+              </h1>
+              <p className="card-summary">
+                {post.content.length > 200
+                  ? post.content.slice(0, 200) + "..."
+                  : post.content}
+              </p>
+            </section>
+            <section className="card-bottom">
+              <section>
+                <span
+                  className="favorites">
+                   ❤
+                  </span>
+              </section>
+              <section className="rating-section">
+                <span className="upvote">⇧</span>
+                <span className="vote-counter">0</span>
+                <span className="downvote">⇩</span>
+              </section>
+            </section>
+          </section>
+        );
+      })}
+    </>
+  );
+};
+
+export default BlogCard;
