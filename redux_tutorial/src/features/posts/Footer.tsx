bc642e829b335e8fc919f3b8bcef696842a24e81
Matt <mhgarry92@gmail.com>
2023-06-07 10:40:55 -0400

    footer made

---
+++ redux_tutorial/src/features/posts/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <p>
+        Made by <a href="https://github.com/mhgarry">Matt</a>
+        <span> referenced and followed the following tutorial<a>https://bejamas.io/blog/learn-modern-react-and-redux-in-2023-by-building-a-blog/?utm_source=Reddit&utm_medium=Social+media+post&utm_campaign=Reddit_Q1_testing</a></span>
+        <span>made by Temitope Olajide<a href="https://github.com/tope-olajide"></a></span>
+      </p>
+    </>
+  )
+}
+
+export default Footer;
