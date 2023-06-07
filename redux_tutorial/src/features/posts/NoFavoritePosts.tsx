26832511450d526e3378858631990346f5fde430
Matt <mhgarry92@gmail.com>
2023-06-07 16:38:56 -0400

    favorites/front end mvp done

--- redux_tutorial/src/features/posts/NoFavoritePosts.tsx
+++ redux_tutorial/src/features/posts/NoFavoritePosts.tsx
@@ -1,9 +1,16 @@
+import { useAppDispatch } from "../../app/hooks";
+import { switchToAllPostsComponent } from "../navigations/navigationSlice";
+
 const NoFavoritePosts = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <section className="no-posts-card">
         <h1>You have not favorited any posts.</h1>
-        <button>Add favorites</button>
+        <button onClick={() => dispatch(switchToAllPostsComponent)}>
+          Add a Favorite!
+        </button>
       </section>
     </>
   );
