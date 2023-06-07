55f150cf6b4eabd92bdd4bcaffab9ace9e1a7cc6
Matt <mhgarry92@gmail.com>
2023-06-07 10:16:34 -0400

    null post component

---
+++ redux_tutorial/src/features/posts/NoPosts.tsx
@@ -0,0 +1,12 @@
+const NoPosts = () => {
+  return (
+    <>
+      <section className="no-posts-card">
+        <h1>You have not created any posts.</h1>
+        <button>Add New</button>
+      </section>
+    </>
+  );
+};
+
+export default NoPosts;
