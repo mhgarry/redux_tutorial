e051c1d999eb85a00d8e7ddcb8ea4572e7b59b7a
Matt <mhgarry92@gmail.com>
2023-06-07 16:31:51 -0400

    add all favorites

--- redux_tutorial/src/features/posts/FavoritePosts.tsx
+++ redux_tutorial/src/features/posts/FavoritePosts.tsx
@@ -1,8 +1,10 @@
+import { useAppSelector } from "../../app/hooks";
 import BlogCard from "./BlogCard";
 import NoFavoritePosts from "./NoFavoritePosts";
+import { selectAllFavoritePosts } from "./postSlice";
 
 const FavoritePosts = () => {
-  const favoritePosts: any[] = [];
+  const favoritePosts = useAppSelector(selectAllFavoritePosts);
   if (!favoritePosts.length) {
     return <NoFavoritePosts />;
   }
--- redux_tutorial/src/features/posts/postSlice.ts
+++ redux_tutorial/src/features/posts/postSlice.ts
@@ -100,6 +100,10 @@ export const postSlice = createSlice({
 
 export const selectAllPosts = (state: RootState) => state.posts;
 
+export const selectAllFavoritePosts = (state: RootState) =>
+   state.posts.allPosts.filter((post) => {
+    return post.isFavorite === true;
+   });
 export const {
   saveBlogPost,
   upvotePost,
