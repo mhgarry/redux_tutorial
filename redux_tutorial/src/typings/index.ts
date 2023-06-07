1b4c54a5fc5fec93ea8f7ca842f27db6df5e7395
Matt <mhgarry92@gmail.com>
2023-06-07 10:46:43 -0400

    defined string types for nav state

--- redux_tutorial/src/typings/index.ts
+++ redux_tutorial/src/typings/index.ts
@@ -5,3 +5,14 @@ export interface IPost {
     voteCount:number,
     isFavorite: boolean,
 } // Define the state type for the data we will store in the store (the posts)
+
+export interface INavigationState {
+  currentComponent:
+  | "AllPostsComponent"
+  | "AddNewPostComponent"
+  | "FavoriteComponent"
+  | "PostComponent"
+  | "EditPostComponent"
+  | "DeletePostComponent";
+} // Defines the currentcomponent property that can be one of five strings (the names of the components)
+
