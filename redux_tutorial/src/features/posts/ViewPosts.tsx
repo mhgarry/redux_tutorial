0567a2cf7ad7d413213539e76b209103c1717aa8
Matt <mhgarry92@gmail.com>
2023-06-07 10:34:24 -0400

    created a component to view selected post

---
+++ redux_tutorial/src/features/posts/ViewPosts.tsx
@@ -0,0 +1,13 @@
+const ViewPost = () => {
+  const post = { title: "", content: ""};
+  return (
+    <>
+      <section className="view-post-container">
+        <h1 className="view-post-title">{post.title}</h1>
+        <p className="view-post-content">{post.content}</p>
+      </section>
+    </>
+  )
+}
+
+export default ViewPost;
