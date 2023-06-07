9c87f10f144d4c94fc103aa4bee0dde4e88e6094
Matt <mhgarry92@gmail.com>
2023-06-07 10:04:27 -0400

    add new post functionality

---
+++ redux_tutorial/src/features/posts/AddNewPost.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { IPost } from "../../typings";
+import PostInputForm from "./PostInputForm";
+
+const AddNewPost = () => {
+  const [title, setTitle] = useState("Blog Title");
+  const [content, setContent] = useState("Blog Content");
+
+  const submitPost = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const post: IPost = {
+      id: Date.now(),
+      title,
+      content,
+      voteCount: 0,
+      isFavorite: false,
+    };
+    console.log(post);
+  };
+  return (
+    <>
+      <PostInputForm
+        submitPost={submitPost}
+        title={title}
+        content={content}
+        setTitle={setTitle}
+        setContent={setContent}
+        />
+    </>
+  );
+};
+
+export default AddNewPost;
